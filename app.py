9db6dd04be843573acf99646061d6b77972c0626
Jonathan Fulk <jonfulk@gmail.com>
2017-02-26 07:41:15 -0600

    Add templates, update logic"

--- app.py
+++ app.py
@@ -1,31 +1,39 @@
-#!bin/python
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from random import randint
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/<choice>', methods=['GET'])
-def player(choice):
-    choice = choice.lower()
-    if choice == 'rock':
-        return computer(1)
-    elif choice == 'paper':
-        return computer(2)
-    elif choice == 'scissors':
-        return computer(3)
-    else:
-        return 'Choose Rock, Paper, or Scissors'
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-def computer(opponent):
-    roll = randint(1, 3)
-    diff = opponent - roll
 
-    if opponent == roll:
-        return 'TIE! Play again?'
-    elif diff == 1 or diff == -2:
-        return 'You win! Play again?'
+@app.route('/<int:user_choice>')
+def chooser(user_choice):
+    winner = None
+    cpu_choice = randint(1, 3)
+    winner = determine_winner(user_choice, cpu_choice)
+    return render_template('results.html', winner=winner, user=user_choice, cpu=cpu_choice)
+
+def determine_winner(player, cpu):
+    """Determine winner by comparing player choice to computer random int.
+
+    Rock = 1, Paper = 2, Scissors = 3
+    Player wins when diff is 1 or -2
+    """
+    if player not in (1, 2, 3):
+        return redirect(url_for('index'))
     else:
-        return 'Sorry, you lose. Try again?'
+        diff = player - cpu
+        if diff == 0:
+            winner = None
+        elif diff == 1 or diff == -2:
+            winner = 'User '
+        else:
+            winner = 'Root '
+        return winner
 
 if __name__ == '__main__':
     app.run(debug=True)
