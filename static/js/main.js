96f4d274280110fa2909f270802a0f1bb4e8143c
Jonathan Fulk <jonfulk@gmail.com>
2017-02-28 16:31:41 -0600

    UI improvements

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,14 @@
+$( document ).ready(function() {
 
-// - highlight on hover
-// - slow down transition from selection to results
-// - use AJAX to do this in only 1 view?
-// - hide divs of what is not chosen before displaying computer choice
+    // - highlight on hover
+    $( "a > img" ).hover(
+        function() {$( this ).addClass( 'outline' )},
+        function() {$( this ).removeClass( 'outline' )}
+    );
+
+    // // - slow down transition from selection to results
+    // - use AJAX to do this in only 1 view?
+    // - hide divs of what is not chosen before displaying computer choice
+
+
+});
